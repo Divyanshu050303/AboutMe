6f5012c5382f19c5d27d9c7ad5eca65777c96297
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-05-19 16:26:21 +0530

    logic part is here

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             EditText.visibility=View.GONE
             DoneButton.visibility=View.GONE
             NickNameText.visibility=View.VISIBLE
+
         }
         val imm=getSystemService(Context.INPUT_METHOD_SERVICE)as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken,0)
